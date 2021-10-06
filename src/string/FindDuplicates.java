483c81ce071cb97058260d3cd8b493b4a884f6d4
ganapathyg981 <ganapathyg981@gmail.com>
2021-10-06 13:33:24 +0530

    Basic code for duplicate string problem

---
+++ src/string/FindDuplicates.java
@@ -0,0 +1,11 @@
+package string;
+
+public class FindDuplicates {
+    public static void main(String[] args) {
+        String input ="geeksforgeeks";
+        printDuplicates(input);
+    }
+    static void printDuplicates(String input){
+
+    }
+}
